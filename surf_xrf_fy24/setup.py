1601859a12ab31fd50d2645356cd076f94e312b0
sonia-ghoshal <sonia.ghoshal@nist.gov>
2024-06-28 11:20:53 -0400

    My first commit

---
+++ surf_xrf_fy24/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Separation of XRF and diffraction data',
+    author='Sonia Ghoshal NIST',
+    license='',
+)
